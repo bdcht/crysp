1c6b5688ffbda19c97b2f5031b6d462fec47a49f
Axel Tillequin <bdcht3@gmail.com>
2012-01-21 16:42:12 +0100

    add Poly class and linear naked DES

---
+++ poly.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+
+from math import *
+from binascii import b2a_hex,a2b_hex
+import struct
+
+
+class Poly(object):
+
+  ival   = None
+  ring   = 0
+
+  @property
+  def size(self):
+      if self.ival:
+          return len(self.ival)
+      return 0
+
+  @size.setter
+  def size(self,size):
+      if self.ival:
+          s=len(self.ival)
+          if size<s:
+              self.ival=self.ival[:size]
+          else:
+              self.ival+=[0]*(size-s)
+      else:
+          self.ival=[0]*size
+  @size.deleter
+  def size(self):
+      self.ival = None
+
+  def __init__(self,v,ring=0,size=None):
+      if isinstance(v,Poly):
+          self.ival = v.ival[:]
+          self.ring = v.ring
+      elif isinstance(v,int):
+          self.size = 1
+          self.ival = [v]
+          if ring: self.ring = ring
+      elif isinstance(v,list):
+          self.size = len(v)
+          if ring: self.ring = ring
+          for i,x in enumerate(v):
+              self.ival[i] = x
+      elif isinstance(v,str):
+        self.size = len(v)
+        self.ring = 256
+        l = map(ord,v)
+        for i,x in enumerate(l):
+            self.ival[i] = x
+      else:
+            NotImplemented
+      if size: self.size = size
+
+
+  def __len__(self):
+    return self.size
+
+  def e(self,i):
+      if self.ring:
+          return self.ival[i]%self.ring
+      else:
+          return self.ival[i]
+
+  def __repr__(self):
+    c = self.__class__
+    r = self.ring
+    l = self.size
+    return '<%s instance with ring=%d (len=%d)>'%(c,r,l)
+
+  def __str__(self):
+    return str(self.ival)
+
+# Iterator for the class. Enables 'for b in self' expressions.
+#------------------------------------------------------------------------------
+  def __iter__(self):
+    for x in range(self.size):
+      yield self.e(x)
+
+# value comparison methods ('==' and '<>' operators).
+#------------------------------------------------------------------------------
+  def __eq__(self,a):
+    if isinstance(a,Poly):
+        return False not in map(lambda x,y:x==y,zip(self,a))
+    return (self.ival==a)
+#------------------------------------------------------------------------------
+  def __ne__(self,a):
+    if isinstance(a,Poly):
+        return False not in map(lambda x,y:x<>y,zip(self,a))
+    return (self.ival<>a)
+
+# getitem operator defines b[i], b[i:j] and b[list] which returns the requested
+# bit values as a long (0L,1L) or a list of such longs.
+#------------------------------------------------------------------------------
+  def __getitem__(self,i):
+    if isinstance(i,int):
+      return Poly(self.e(i),self.ring)
+    elif isinstance(i,slice):
+      return Poly(self.ival[i],self.ring)
+    else:
+      s=[]
+      for x in i:
+          s.append(self.e(x))
+      return Poly(s,self.ring)
+
+# setitem operator defines b[i], b[i:j] and b[list] which allow to affect new
+# values to these bits, from another object, int/long value or a bit list.
+#------------------------------------------------------------------------------
+  def __setitem__(self,i,v):
+    if isinstance(i,int):
+        if self.ring:
+            self.ival[i] = v%self.ring
+        else:
+            self.ival[i] = v
+    else:
+      if isinstance(i,slice):
+        start,stop,step = i.indices(self.size)
+        r = range(start,stop,step)
+      else:
+        r = i
+      try:
+        assert len(r)==len(v)
+        for j,b in zip(r,v):
+          self[j] = b
+      except (TypeError,AssertionError):
+        for j,b in zip(r,Poly(v,self.ring,len(r))):
+          self[j] = b
+
+# unary bitwise operators. The result is a new object which has same length.
+#------------------------------------------------------------------------------
+  def __lshift__(self,i):
+    res = Poly(self)
+    res.ival = [0]*i + res.ival
+    return res
+  def __rshift__(self,i):
+    res = Poly(self)
+    res.ival = res.ival[i:]
+    return res
+
+# binary operators, rvalue and lvalue implementations.
+# (Note that resulting object length is max length.
+#------------------------------------------------------------------------------
+  def __and__(self,rvalue):
+    if not isinstance(rvalue,Poly):
+      obj = Poly(rvalue,ring=self.ring)
+    else:
+      obj = rvalue
+    return Poly(map(lambda x,y: x&y, zip(self.ival,obj.ival)),self.ring)
+  def __or__(self,rvalue):
+    if not isinstance(rvalue,Poly):
+      obj = Poly(rvalue,ring=self.ring)
+    else:
+      obj = rvalue
+    return Poly(map(lambda x,y: x|y, zip(self.ival,obj.ival)),self.ring)
+  def __xor__(self,rvalue):
+    if not isinstance(rvalue,Poly):
+      obj = Poly(rvalue,ring=self.ring)
+    else:
+      obj = rvalue
+    return Poly(map(lambda x,y: x^y, zip(self.ival,obj.ival)),self.ring)
+
+  def __rand__(self,lvalue):
+    return (self & lvalue)
+  def __ror__(self,lvalue):
+    return (self | lvalue)
+  def __rxor__(self,lvalue):
+    return (self ^ lvalue)
+
+# operator // is used for concatenation:
+  def __floordiv__(self,rvalue):
+    if not isinstance(rvalue,Poly):
+      obj = Poly(rvalue,self.ring)
+    else:
+      obj = rvalue
+    return Poly(self.ival+obj.ival,self.ring)
+
+  def poly(self):
+    if self.ring: return [x%self.ring for x in self.ival]
+    return self.ival[:]
+
--- wb.py
+++ wb.py
@@ -3,6 +3,7 @@
 import pickle
 import random
 from des import *
+from poly import Poly
 
 # -----------------------------------------------------------------------------
 # Electronic Code Book mode of Operation
@@ -87,4 +88,159 @@ class WhiteBoxDes(ECB):
             fout[ro:nro] = self.KS[r][n][x.ival]
             ri,ro = nri,nro
         return P(fout)
+
+# -----------------------------------------------------------------------------
+# The naked DES:
+
+def table_rKT(r,K):
+    rks = table_rKS(r,K)
+    rkt = []
+    for n in range(12):
+        rkt.append(range(256))
+    for v in range(256):
+        re = Bits(v,8)
+        for n in range(8):
+            x = Bits(rks[n][re[0:6].ival],4)//re[(0,5,6,7)]
+            rkt[n][re.ival] = x.ival
+    for n in range(12):
+        rkt[n] = tuple(rkt[n])
+    return rks,tuple(rkt)
+
+def getrbits_T_in():
+    r = E(Poly(range(32))).ival
+    sr = set(range(32))
+    rbits=[]
+    for i in range(8):
+        sr.remove(r[0])
+        sr.remove(r[5])
+        rbits += [r[0],r[5]]
+        r = r[6:]
+    return rbits+list(sr)
+
+def table_M1():
+    l,r = range(32),Poly(range(32,64))
+    re = E(r).ival
+    rbits = r.ival
+    blk = []
+    for b in range(12):
+        blk.append([None]*8)
+        if b<8:
+            blk[b][0:6]=re[0:6]
+            rbits.remove(re[0])
+            rbits.remove(re[5])
+            re = re[6:]
+            blk[b][6:8]=l[0:2]
+            l = l[2:]
+        else:
+            blk[b][0:4]=l[0:4]
+            blk[b][4:8]=rbits[0:4]
+            l = l[4:]
+            rbits = rbits[4:]
+    assert len(rbits)==0
+    assert len(l)==0
+    table = []
+    for b in range(12):
+        table.extend(blk[b])
+    M = Poly(range(64))
+    return IP(M)[table].ival
+
+def SRLRformat():
+    I = Poly(range(96))
+    SR = Poly([None]*32)
+    L = Poly([None]*32)
+    R = Poly([None]*32)
+    rbits = getrbits_T_in()
+    for i in range(0,8):
+        s = I[8*i:8*i+8].ival
+        SR[4*i:4*i+4]= s[:4]
+        R[rbits[:2]] = s[4:6]
+        L[2*i:2*i+2] = s[6:8]
+        rbits = rbits[2:]
+    for i in range(8,12):
+        s = I[8*i:8*i+8].ival
+        L[4*i-16:4*i-12] = s[:4]
+        R[rbits[:4]] = s[4:]
+        rbits = rbits[4:]
+    return SR,L,R
+
+def ERLRformat():
+    I = Poly(range(96))
+    ER = Poly([None]*48)
+    L = Poly([None]*32)
+    R = Poly([None]*32)
+    rbits = getrbits_T_in()
+    for i in range(0,8):
+        s = I[8*i:8*i+8].ival
+        ER[6*i:6*i+6]= s[:6]
+        R[rbits[:2]] = [s[0],s[5]]
+        L[2*i:2*i+2] = s[6:8]
+        rbits = rbits[2:]
+    for i in range(8,12):
+        s = I[8*i:8*i+8].ival
+        L[4*i-16:4*i-12] = s[:4]
+        R[rbits[:4]] = s[4:]
+        rbits = rbits[4:]
+    return ER,L,R
+
+def table_M2():
+    Mat = [Bits(0,96) for i in range(96)]
+    SR,L,R = SRLRformat()
+    newL = R.ival
+    newR = Poly(zip(P(SR),L))
+    RE = E(newR).ival
+    m=[]
+    for r in range(8):
+        m += RE[0:6]+newL[0:2]
+        RE = RE[6:]
+        newL  = newL[2:]
+    rbits = getrbits_T_in()[16:]
+    for r in range(4):
+        m += newL[0:4]
+        for b in rbits[:4]:
+            m += [newR.e(b)]
+        newL = newL[4:]
+        rbits = rbits[4:]
+    assert len(m)==96
+    for v in range(96):
+        l = m[v]
+        try:
+            for x in l: Mat[v][x]=1
+        except TypeError:
+            Mat[v][l]=1
+        Mat[v] = Mat[v].ival
+    return Mat,m
+
+def table_M3():
+    ER,L,R = ERLRformat()
+    C = Poly(R.ival+L.ival)
+    return IPinv(C).ival
+
+class WhiteDES(ECB):
+
+    def __init__(self,KT,tM1,tM2,tM3):
+        self.KT = KT
+        self.tM1 = tM1
+        self.tM2 = tM2
+        self.tM3 = tM3
+
+    def FX(self,v):
+        res = Bits(0,96)
+        for b in range(96):
+            res[b] = (v&self.tM2[b]).hw()%2
+        return res
+
+    def _cipher(self,M,d):
+        assert M.size==64
+        if d==1:
+            blk = M[self.tM1]
+            for r in range(16):
+                t = 0
+                for n in range(12):
+                    nt = t+8
+                    blk[t:nt] = self.KT[r][n][blk[t:nt].ival]
+                    t = nt
+                blk = self.FX(blk)
+            return blk[self.tM3]
+        if d==-1:
+            raise NotImplementedError
 
