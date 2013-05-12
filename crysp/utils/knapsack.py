bcdba012527aae931c1f6acbe569f05a15d0d6a7
Axel Tillequin <bdcht3@gmail.com>
2013-05-12 16:10:18 +0200

    moved some old stuff inside crysp

---
+++ crysp/utils/knapsack.py
@@ -0,0 +1,40 @@
+# for a list l of couples (object,weight), return a
+# list of unique couples with sum of weights equal s. 
+# result is not "optimal" by any means, it is just the
+# first recursive solution encountered.
+def exactsum(l,s,i=0,r=[]):
+    n = len(l)
+    if s==0: return True
+    if s<0 or i==n: return False
+    if exactsum(l,s-l[i][1],i+1):
+        r.append(l[i])
+        return True if i else r
+    else:
+        return exactsum(l,s,i+1)
+
+# a simple version of dynamic programming method
+# to find a minimal-length list of couples from l
+# with sum of weights equal to s.
+def dynprog(l,s):
+    n = len(l)
+    p = {}
+    p[0] = [0]
+    for x in range(1,s+1):
+        m = None
+        for i in range(n):
+            u = x-l[i][1]
+            if (u>=0 and (u in p) and (p[u][0]<m or m==None)):
+                    m = p[u][0]
+                    im = i
+        if m!=None:
+            p[x] = [m+1]
+            src  = p[x-l[im][1]]
+            for j in range(1,m+1): p[x].append(src[j])
+            p[x].append(l[im])
+        else:
+            if x in p:
+                del p[x]
+    try:
+        return p[s][1:]
+    except KeyError:
+        return None
---
+++ crysp/utils/perms.py
@@ -0,0 +1,59 @@
+# recursive iterator over the group of (n-k)! permutations
+# of the last n-k elements of list l (k-deep permutations).
+# k=0 returns ALL n! permutations
+def permutk(l,k):
+    assert k>=0
+    if k>=len(l):
+        yield l[:]
+    for i in range(k,len(l)):
+        tmp = l[i]
+        for j in range(i,k,-1):
+            l[j] = l[j-1]
+        l[k] = tmp
+        for p in permutk(l,k+1): yield p
+        for j in range(k,i): l[j] = l[j+1]
+        l[i] = tmp
+
+# inplace permute list l to "next" permutation
+# according to the natural order induced by
+# comparisons of its elements.
+def nextperm(l):
+    k = len(l)-2
+    while (k>=0 and l[k]>l[k+1]): k -= 1
+    lpos = k+1
+    rpos=len(l)-1
+    while lpos<rpos:
+        l[lpos],l[rpos] = l[rpos],l[lpos]
+        lpos += 1
+        rpos -= 1
+    if k==-1:
+        return l
+    i = k+1
+    while (l[i]<l[k]): i+=1
+    l[i],l[k] = l[k],l[i]
+    return l
+
+# recursive iterator providing k-deep combinations of
+# p elements of list l. The combinations order is induced
+# by indices of the elements in l.
+def combink(l,p,k):
+    assert k>=0
+    n = len(l)
+    assert 0<p<=n
+    # create internal "static" variable:
+    if not hasattr(combink,'r'):
+        combink.r = range(n)+[-1]
+    if k<p:
+        #print '\t'*k + "k=%d, loop:[%d,%d]"%(k,combink.r[k-1]+1,n-p+k+1)
+        for i in range(combink.r[k-1]+1, n-p+k+1):
+            combink.r[k]=i
+            for x in combink(l,p,k+1):
+                yield x
+        # if recursion is back to initial call
+        # then cleanup internal static variable:
+        if k==0: del combink.r
+    else:
+        #print '\t'*k + 'k=%d, r='%k,combink.r,'yield =>',
+        yield [l[i] for i in combink.r[:p]]
+
+
