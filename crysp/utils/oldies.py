cd43d29dbec936e7f33410998e813f8be4dd472b
Axel Tillequin <bdcht3@gmail.com>
2015-05-29 16:28:43 +0200

    initial commit of Substitution/Transposition utilities

---
+++ crysp/utils/oldies.py
@@ -0,0 +1,18 @@
+class Substitution(object):
+    def __init__(self,data=None):
+        self.key = ['-']*256
+        self.data = data
+    def setkey(self,c,v):
+        self.key[ord(c)]=v
+    def getkey(self,c):
+        return chr(self.key.index(c))
+    def __call__(self,data=None):
+        if data is None:
+            data = self.data
+        if data is None:
+            return
+        kstr=''.join(self.key)
+        return data.translate(kstr)
+
+class Transposition(object):
+    pass
