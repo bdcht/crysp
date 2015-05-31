b2fdb0bba62e5d994bd5c80f7f1a040145edb40f
Axel Tillequin <bdcht3@gmail.com>
2015-05-31 18:44:41 +0200

    add serpent test script

--- tests/test-serpent.py
+++ tests/test-serpent.py
@@ -1,11 +1,31 @@
+#!/usr/bin/env python
 from crysp.serpent import *
 
+# test Serpent block cipher from NESSIE test vectors:
+# ---------------------------------------------------
+
+# set 1, vector 0:
+K = Bits("8000000000000000000000000000000000000000000000000000000000000000".decode('hex'),bitorder=1)
+P = "00000000000000000000000000000000".decode('hex')
 S = Serpent(K)
+C = S.enc(P)
+assert C.encode('hex')=="A223AA1288463C0E2BE38EBD825616C0".lower()
+assert S.dec(C)==P
 
-inputstr = "000102030405060708090a0b0c0d0e0fff0102030405060708090a0b0c0d0e0f"
+# set 1, vector 1:
+K = Bits("4000000000000000000000000000000000000000000000000000000000000000".decode('hex'),bitorder=1)
+P = "00000000000000000000000000000000".decode('hex')
+S = Serpent(K)
+C = S.enc(P)
+assert C.encode('hex')=="EAE1D405570174DF7DF2F9966D509159".lower()
+assert S.dec(C)==P
 
-#Serpent/CBC/PKCS7Padding :
-output1 = "f8940ca31aba8ce1e0693b1ae0b1e08daef6de03c80f019774280052f824ac44540bb8dd74dfad47f83f9c7ec268ca68"
-#Serpent/CBC/WithCTS",
-output2 = "aef6de03c80f019774280052f824ac44f8940ca31aba8ce1e0693b1ae0b1e08d"
+# set 1, vector 2:
+K = Bits("1111111111111111111111111111111111111111111111111111111111111111".decode('hex'),bitorder=1)
+P = "11111111111111111111111111111111".decode('hex')
+S = Serpent(K)
+C = S.enc(P)
+assert C.encode('hex')=="A482EAA5D5771F2FDB2EA1A5F141B9E2".lower()
+assert S.dec(C)==P
+
 
