b92efaaccacdb31b04df533327248d6348b7e6af
Axel Tillequin <bdcht3@gmail.com>
2018-06-07 22:03:19 +0200

    add rc4 test

--- tests/test_rc4.py
+++ tests/test_rc4.py
@@ -4,6 +4,9 @@ from crysp.rc4 import *
 
 def test_rc4_001():
     k = b'Key'
+    X = RC4(k)
+    assert X.S.ival[0:4] == [75, 51, 132, 157]
+    assert X.keystream(16).ival == [235, 159, 119, 129, 183, 52, 202, 114, 167, 25, 74, 40, 103, 182, 66, 149]
     assert RC4(k).enc(b'Plaintext') == codecs.decode(b'BBF316E8D940AF0AD3','hex')
 
 def test_rc4_002():
